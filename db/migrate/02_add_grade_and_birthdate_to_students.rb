87fcd1dbbe20549a2782d31eadb058accdc5445c
Erick Castille <ecastille924@gmail.com>
2020-11-19 01:02:14 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,9 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
   def change 
-    add_column :students :grade
+    add_column :students, :grade, :integer
+  end
+  
+  def change 
+    add column :students, :birthdate, :string
   end
 end
